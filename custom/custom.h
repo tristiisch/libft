d458f9ea127c0bda588bc4a0ea79ab0ea4abd326
tglory <tglory@student.42Lyon.fr>
2021-12-07 05:51:08 +0100

    miss ft_strjoin_plus

--- custom/custom.h
+++ custom/custom.h
@@ -6,7 +6,7 @@
 /*   By: tglory <tglory@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 22:07:01 by tglory            #+#    #+#             */
-/*   Updated: 2021/12/07 03:40:41 by tglory           ###   ########lyon.fr   */
+/*   Updated: 2021/12/07 05:50:53 by tglory           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,20 @@
 # include "libft.h"
 
 /*-------------------------------{ BASICS }-------------------------------*/
-int					ft_isequals_ignore(char *str1, char *str2);
-int					ft_isequals(char *str1, char *str2);
-int					ft_isblank(char *str);
-void				*ft_realloc(void *ptr, size_t new_size, size_t old_size);
+int		ft_isequals_ignore(char *str1, char *str2);
+int		ft_isequals(char *str1, char *str2);
+int		ft_isblank(char *str);
+void	*ft_realloc(void *ptr, size_t new_size, size_t old_size);
 
 /*-------------------------------{ CUSTOM }-------------------------------*/
-void				*ft_lstget(t_list *lst, char *key,
-						char *(*get_key) (void *));
-int					ft_lstremove(t_list *lst, char *key,
-						char *(*get_key) (void *), void (*f) (void *));
+void	*ft_lstget(t_list *lst, char *key,
+			char *(*get_key) (void *));
+int		ft_lstremove(t_list *lst, char *key,
+			char *(*get_key) (void *), void (*f) (void *));
 
 /*--------------------------------{ PERSO }--------------------------------*/
-char				**ft_strtrunc(char *str, char c);
+char	**ft_strtrunc(char *str, char c);
+char	*ft_strjoin_plus(char const *s1, char const *s2,
+			char const *s3);
 
 #endif
